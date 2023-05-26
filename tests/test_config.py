9c37a3e4cf1a49dd5ccfb5ac6e4c2e5412efe572
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-05-26 10:59:03 -0700

    baseline

---
+++ tests/test_config.py
@@ -0,0 +1,13 @@
+import os
+from k2eg import k2eg
+
+def test_k2eg_get():
+    try:
+        # test env variable configuration
+        os.environ["K2EG_KAFKA_BROKER_URL"] = 'localhost:1234'
+        k = k2eg()
+        assert k.settings.KAFKA_BROKER_URL == "localhost:1234"
+    finally:
+        del os.environ["K2EG_KAFKA_BROKER_URL"]
+    
+    
---
+++ tests/test_k2eg.py
@@ -0,0 +1,5 @@
+from k2eg import k2eg
+
+def test_k2eg_get():
+    k = k2eg()
+    assert k.get("pv_name") == "pv_name"

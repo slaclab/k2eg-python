0b90c6de5a820b674a7b5f9b837a8a7138294abb
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-05-30 00:10:13 +0000

    complete monitor message

--- k2eg/k2eg.py
+++ k2eg/k2eg.py
@@ -4,6 +4,9 @@ import threading
 from readerwriterlock import RWLock
 from kafka import KafkaConsumer
 from kafka import KafkaProducer
+import re
+import json
+
 class k2eg:
     """K2EG client"""
     def __init__(self):
@@ -14,7 +17,10 @@ class k2eg:
             )
         self.__lock = RWLock()
         self.__consumer = KafkaConsumer(bootstrap_servers=self.settings.kafka_broker_url)
-        self.__producer = KafkaProducer(bootstrap_servers=self.settings.kafka_broker_url)
+        self.__producer = KafkaProducer(
+            bootstrap_servers=self.settings.kafka_broker_url,
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
         self.__thread = threading.Thread(target=self.__consumer_handler, args=(1,))
         self.__thread.start()
         self.__consume_data = True
@@ -59,12 +65,21 @@ class k2eg:
         for msg in self.__consumer:
            self.__process_message(msg)
             
+    def __check_pv_name(pv_name):
+        pattern = r'^[a-zA-Z0-9:]+$'
+        if re.match(pattern, pv_name):
+            return True
+        else:
+            return False
+
+    def __normalize_pv_name(pv_name):
+        return pv_name.replace(":", "_")
 
     def get(self, pv_name):
         logging.info("Get for pv {}".format(pv_name))
         return pv_name
     
-    def monitor(self, pv_name, handler):
+    def monitor(self, pv_name, protocol, handler):
         """ Add a new monitor for pv if it is not already activated
         Parameters
                 ----------
@@ -72,11 +87,45 @@ class k2eg:
                     The name of the PV to monitor
                 handler: function
                     The handler to be called when a message is received
+        Rais:
+                ----------
+                True: the monitor has been activated
+                False: otherwhise
         """
+        if not self.____check_pv_name(pv_name):
+            raise ValueError("The PV name can only containes letter (upper or lower), number ad the character ':'")
+        
+        if protocol.lower() != "pva" and protocol.lower() != "ca":
+             raise ValueError("The portocol need to be one of 'pva'  'ca'")
+        
+        topics = []
         with self.__lock.get_wlock():
             if self.__monitor_pv_handler.has_key(pv_name):
                 logging.info("MOnitor already activate for pv {}".format(pv_name))
                 return
             self.__monitor_pv_handler[pv_name] = handler
+            #subscribe to all needed topic
+            for pv in self.__monitor_pv_handler:
+                # create topic name from the pv one
+                topics.append(self.__normalize_pv_name(pv))
+            logging.debug("start subscribtion on topics {}".format(topics))
+            self.__consumer.subscribe(topics)
+
+        # send message to k2eg from activate (only for last topics) monitor(just in case it is not already activated)
+        monitor_json_msg = {
+            "command": "monitor",
+            "serialization": "msgpack",
+            "protocol": protocol.lower(),
+            "pv_name": pv_name,
+            "dest_topic": self.__normalize_pv_name(pv),
+            "activate": True
+        }
+        # send message to k2eg
+        self.__producer.send(
+            self.settings.k2eg_cmd_topic, 
+            value=monitor_json_msg
+            )
+        self.__producer.flush()
+        return True
         
         

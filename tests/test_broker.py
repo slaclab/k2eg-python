ebdbcd20c2ddc397c0aee04e351c4669f4276be2
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-06-30 15:52:12 -0700

    create docker environment for tests

--- tests/test_broker.py
+++ tests/test_broker.py
@@ -1,5 +1,30 @@
+import time
 import pytest
 from k2eg.broker import Broker as broker
+from confluent_kafka.admin import AdminClient, NewTopic
+
+
+@pytest.fixture(scope='session', autouse=True)
+def my_fixture():
+    # Setup code goes here
+    admin_client = AdminClient({
+        "bootstrap.servers": "kafka:9092"
+    })
+
+    topic_list = [
+           NewTopic("reply-topic-xyz", num_partitions=1, replication_factor=1),
+           NewTopic("cmd-in-topic", num_partitions=1, replication_factor=1),
+           ]
+    admin_client.create_topics(topic_list)
+    # Wait until the topic appears in the cluster (with a timeout)
+    for i in range(10):
+        cluster_topics = admin_client.list_topics().topics
+        if "reply-topic-xyz" in cluster_topics and  "cmd-in-topic" in cluster_topics:
+            print("Topicshas been created.")
+            break
+        time.sleep(1)
+    else:
+        print(f"Topic was not created within the timeout period.")
 
 def test_broker_bad_conf():
     with pytest.raises(ValueError, 

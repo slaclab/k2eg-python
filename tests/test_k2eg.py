a6a969f2c0f7dea1de4d936ff1830ac01fd9700b
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-08-17 16:36:35 +0000

    add cli __pycache__ to gitingore

--- tests/test_k2eg.py
+++ tests/test_k2eg.py
@@ -4,30 +4,6 @@ import time
 import pytest
 from confluent_kafka.admin import AdminClient, NewTopic
 
-
-# @pytest.fixture(scope='session', autouse=True)
-# def my_fixture():
-#     print("Prepare kafka topics for test.")
-#     # Setup code goes here
-#     admin_client = AdminClient({
-#         "bootstrap.servers": "kafka:9092"
-#     })
-
-#     topic_list = [
-#            NewTopic("reply-topic-xyz", num_partitions=1, replication_factor=1),
-#            NewTopic("cmd-in-topic", num_partitions=1, replication_factor=1),
-#            ]
-#     admin_client.create_topics(topic_list)
-#     # Wait until the topic appears in the cluster (with a timeout)
-#     for i in range(10):
-#         cluster_topics = admin_client.list_topics().topics
-#         if "reply-topic-xyz" in cluster_topics and  "cmd-in-topic" in cluster_topics:
-#             print("Topics has been created.")
-#             break
-#         time.sleep(1)
-#     else:
-#         print("Topic was not created within the timeout period.")
-
 def test_k2eg_get():
     k = k2eg('test', 'app-test')
     get_value = k.get('channel:ramp:ramp', 'pva')

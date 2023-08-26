d9be8d387eecd9116cfe3e20adf384f2ee8740b8
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-08-26 04:36:54 +0000

    test now uses a unique ke2g instance to reduce the time for kafka to reorder his stuff, the reply topic now match the name of the app + '_reply' postfix

--- tests/test_dml.py
+++ tests/test_dml.py
@@ -3,6 +3,19 @@ from k2eg.dml import _filter_pv_uri
 import time
 import pytest
 
+k: k2eg.dml = None
+
+@pytest.fixture(scope="session", autouse=True)
+def my_setup(request):
+    print('Enable k2eg')
+    global k
+    k = k2eg.dml('test', 'app-test')
+    def fin():
+        global k
+        if k is not None:
+              k.close()
+    request.addfinalizer(fin)
+
 def test_uri_protocol_ioc_attribute():
     protocol, resource = _filter_pv_uri('pva://ioa:a.attribute')
     assert protocol == 'pva', "protocol should not be None"
@@ -19,65 +32,35 @@ def test_uri_protocol_ioc():
     assert resource == 'ioa:a', "protocol should not be None"
 
 def test_exception_on_get_without_protocol():
-    k = k2eg.dml('test', 'app-test')
-    try:
-        with pytest.raises(ValueError, 
-                       match=r".*url.*not.*formed"):
-                       k.get('//bad:pv:name', timeout=0.5)
-    finally:
-        if k is not None: 
-              k.close()
+    with pytest.raises(ValueError, 
+                    match=r".*url.*not.*formed"):
+                    k.get('//bad:pv:name', timeout=0.5)
+
 
 def test_exception_on_get_with_bad_pv_name():
-    k = k2eg.dml('test', 'app-test')
-    try:
-        with pytest.raises(ValueError, 
-                       match=r".*url.*not.*formed"):
-                       k.get('pva://', timeout=0.5)
-    finally:
-        if k is not None: 
-              k.close()
+    with pytest.raises(ValueError, 
+                    match=r".*url.*not.*formed"):
+                    k.get('pva://', timeout=0.5)
 
 def test_exception_on_get_with_bad_protocol():
-    k = k2eg.dml('test', 'app-test')
-    try:
-        with pytest.raises(ValueError, 
-                       match=r".*url.*not.*formed"):
-                       k.get('unkonwn://', timeout=0.5)
-    finally:
-        if k is not None:
-            k.close()
+    with pytest.raises(ValueError, 
+                    match=r".*url.*not.*formed"):
+                    k.get('unkonwn://', timeout=0.5)
 
 def test_k2eg_get():
-    k = k2eg.dml('test', 'app-test')
-    try:
-        get_value = k.get('pva://channel:ramp:ramp')
-        assert get_value is not None, "value should not be None"
-    finally:
-        if k is not None: 
-              k.close()
+    get_value = k.get('pva://channel:ramp:ramp')
+    assert get_value is not None, "value should not be None"
 
 def test_k2eg_get_timeout():
-    k = k2eg.dml('test', 'app-test')
-    try:
-         with pytest.raises(k2eg.OperationTimeout, 
-                       match=r"Timeout.*"):
-                       k.get('pva://bad:pv:name', timeout=0.5)
-    finally:
-        if k is not None: 
-              k.close()
+    with pytest.raises(k2eg.OperationTimeout, 
+                match=r"Timeout.*"):
+                k.get('pva://bad:pv:name', timeout=0.5)
 
 def test_k2eg_get_bad_pv():
-    k = k2eg.dml('test', 'app-test')
-    try:
-        with pytest.raises(k2eg.OperationError):
-                        k.get('pva://bad:pv:name')
-    finally:
-        if k is not None: 
-              k.close()
+    with pytest.raises(k2eg.OperationError):
+                    k.get('pva://bad:pv:name')
 
 def test_k2eg_monitor():
-    k = k2eg.dml('test', 'app-test')
     try:
         received_message = None
 
@@ -91,12 +74,9 @@ def test_k2eg_monitor():
         
         assert received_message is not None, "value should not be None"
     finally:
-        if k is not None: 
-            k.stop_monitor('channel:ramp:ramp')
-            k.close()
+        k.stop_monitor('channel:ramp:ramp')
 
 def test_k2eg_monitor_stop_restart():
-    k = k2eg.dml('test', 'app-test')
     try:
         received_message = None
 
@@ -118,40 +98,28 @@ def test_k2eg_monitor_stop_restart():
         
         assert received_message is not None, "value should not be None"
     finally:
-        if k is not None: 
-            k.stop_monitor('channel:ramp:ramp')
-            k.close()
+        k.stop_monitor('channel:ramp:ramp')
+
 
 def test_put():
-    k = k2eg.dml('test', 'app-test')
-    try:
-        k.put("pva://variable:a", 0)
-        k.put("pva://variable:b", 0)
-        time.sleep(1)
-        res_get = k.get("pva://variable:sum")
-        assert res_get['value'] == 0, "value should not be 0"
-        k.put("pva://variable:a", 2)
-        k.put("pva://variable:b", 2)
-        #give some time to ioc to update
-        time.sleep(1)
-        res_get = k.get("pva://variable:sum")
-        assert res_get['value'] == 4, "value should not be 0"
-    finally:
-        k.close()
+    k.put("pva://variable:a", 0)
+    k.put("pva://variable:b", 0)
+    time.sleep(1)
+    res_get = k.get("pva://variable:sum")
+    assert res_get['value'] == 0, "value should not be 0"
+    k.put("pva://variable:a", 2)
+    k.put("pva://variable:b", 2)
+    #give some time to ioc to update
+    time.sleep(1)
+    res_get = k.get("pva://variable:sum")
+    assert res_get['value'] == 4, "value should not be 0"
 
 def test_put_timeout():
-    k = k2eg.dml('test', 'app-test')
-    try:
-        with pytest.raises(k2eg.OperationTimeout, 
-                        match=r"Timeout.*"):
-                        k.put("pva://bad:pv:name", 0, timeout=0.5)
-    finally:
-        k.close()
+    with pytest.raises(k2eg.OperationTimeout, 
+                    match=r"Timeout.*"):
+                    k.put("pva://bad:pv:name", 0, timeout=0.5)
+
 
 def test_put_wrong_device_timeout():
-    k = k2eg.dml('test', 'app-test')
-    try:
-        with pytest.raises(k2eg.OperationError):
-                        k.put("pva://bad:pv:name", 0)
-    finally:
-        k.close()
+    with pytest.raises(k2eg.OperationError):
+                    k.put("pva://bad:pv:name", 0)
